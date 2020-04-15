e25260d882009e36fbc2a65fb560af09d59a41f9
Caleb Brenner <calebwill91@gmail.com>
2020-04-15 02:43:01 -0500

    Used HTTP Client to implement GET and POST functionality. Hit errors with jsonplaceholder website likely due to modern modules and browser issues with CORS. Will revisit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent,
+    PostsComponent,
+    PostFormComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
